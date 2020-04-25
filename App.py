057631c45091bc6b41d041ff27476b8ba7bba6d6
Torge Wolff <wolff.torge@gmail.com>
2020-04-25 20:17:21 +0200

    added read entry fields.

--- App.py
+++ App.py
@@ -7,16 +7,21 @@ from Agents.Indiuvidual import Indiuvidual
 class MyApp:
 
     def __init__(self, title, num_balls, ball_size, max_ball_speed):
-        self.Height = 1024
-        self.Width = 1024
-        self.ui = GUI(Height=self.Height, Width=self.Width, title=title)
+        self.Height = 1200
+        self.Width = 1200
+        self.ui = GUI(Height=self.Height, Width=self.Width, title=title, btn1_fnct=self.init_ui, btn3_fnct=self.start,
+                      btn2_fnct=self._delete_individuals)
         self.num_balls = num_balls
         self.ball_size = ball_size
         self.max_ball_speed = max_ball_speed
         self.individuals = []
-        self.init_ui()
 
     def init_ui(self):
+
+        self.num_balls = int(self.ui.num_balls_entry.get())
+        self.ball_size = int(self.ui.ball_size_entry.get())
+        self.max_ball_speed = int(self.ui.ball_speed_entry.get())
+
         for ball in range(self.num_balls):
             height_rand = np.random.randint(0, self.Height - self.ball_size)
             width_rand = np.random.randint(0, self.Width - self.ball_size)
@@ -27,6 +32,11 @@ class MyApp:
                                 xspeed=rand_speed, yspeed=rand_speed)
 
             self.individuals.append(agent)
+
+    def _delete_individuals(self):
+        self.ui.canvas.delete('all')
+        self.individuals = None
+        self.individuals = []
 
     def start(self):
         self.ui.start_animation()
--- UI/GUI.py
+++ UI/GUI.py
@@ -4,14 +4,54 @@ import numpy as np
 
 
 class GUI:
-    def __init__(self, Height, Width, title):
+    def __init__(self, Height, Width, title, btn1_fnct, btn2_fnct, btn3_fnct):
         self.tk = Tk()
         self.active = True
-        self.canvas = Canvas(self.tk, width=Width, height=Height)
+        self._init_upper_ui()
+        self._init_canvas(Height, Width)
+        self._init_upper_button(btn1_fnct, btn2_fnct, btn3_fnct)
         self.tk.title(title)
-        self.canvas.pack()
+        self.tk.geometry('{}x{}'.format(Height, Width))
+
+    def _init_upper_ui(self):
+        self.num_balls_text = StringVar(value=100)
+        self.num_balls_label = Label(self.tk, text='Individuals:', font=('bold', 14), pady=20)
+        self.num_balls_label.grid(row=0, column=0, sticky=E)
+        self.num_balls_entry = Entry(self.tk, textvariable=self.num_balls_text)
+        self.num_balls_entry.grid(row=0, column=1, sticky=E)
+
+        self.ball_speed_text = StringVar(value=5)
+        self.ball_speed_label = Label(self.tk, text='Max. Ball Speed:', font=('bold', 14), pady=20, padx=20)
+        self.ball_speed_label.grid(row=0, column=2, sticky=E)
+        self.ball_speed_entry = Entry(self.tk, textvariable=self.ball_speed_text)
+        self.ball_speed_entry.grid(row=0, column=3, sticky=E)
+
+        self.ball_size_text = StringVar(value=10)
+        self.ball_size_label = Label(self.tk, text='Ball Size:', font=('bold', 14), pady=20, padx=20)
+        self.ball_size_label.grid(row=0, column=4, sticky=E)
+        self.ball_size_entry = Entry(self.tk, textvariable=self.ball_size_text)
+        self.ball_size_entry.grid(row=0, column=5, sticky=E)
+
+    def _init_upper_button(self, btn1_fnct, btn2_fnct, btn3_fnct):
+        self.load_config_btn = Button(master=self.tk, text='Load Config', command=btn1_fnct)
+        self.load_config_btn.grid(row=1, column=0)
+
+        self.reset_config_btn = Button(master=self.tk, text='Reset Config', command=btn2_fnct)
+        self.reset_config_btn.grid(row=1, column=1)
+
+        self.start_animation_btn = Button(master=self.tk, text='Start Animation', command=btn3_fnct)
+        self.start_animation_btn.grid(row=1, column=2)
+
+    def _init_canvas(self, Height, Width):
+        self.canvas = Canvas(self.tk, width=Width - (Width / 100 * 10), height=Height - (Height / 100 * 10),
+                             highlightthickness=2, highlightbackground="black")
+        self.canvas.grid(row=2, column=0, columnspan=6, rowspan=6, pady=10, padx=20)
+
+    def _clear_canvas(self):
+        self.canvas.delete("all")
 
     def start_animation(self):
+        print('Hi!!')
         while self.active:
             self.tk.update_idletasks()
             self.tk.update()
